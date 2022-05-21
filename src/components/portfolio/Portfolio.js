6187f36694ccd319ce9522917cdefe71f5f3a853
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-05-21 23:30:07 +0300

    done with the props, will delete the hard code, is a fallback

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,25 +1,24 @@
 import React from "react";
 import "./portfolio.css";
+import PortfolioItem from "./PortfolioItem";
 import one from "../../assets/portfolio/1.jpg";
 
 const Portfolio = () => {
-  const data = {
-    image: { one },
-    itemTitle: "education course website",
-    viewBtnPara: "view project",
-    description:
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque",
-    listItems: [
-      "Created - ",
-      "4 Dec 20",
-      "technologies used - ",
-      "Html, Css",
-      "Role - ",
-      "Frontend",
-      "View Online - ",
-      "www.domain.com",
-    ],
-  };
+  const data = [
+    {
+      image: { one },
+      itemTitle: "education course website",
+      viewBtnPara: "view project",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque",
+      listItems: [
+        { key: "Created - ", value: "4 Dec 2020" },
+        { key: "technologies used - ", value: "Html, Css" },
+        { key: "Role - ", value: "Frontend" },
+        { key: "View Online - ", value: "www.domain.com" },
+      ],
+    },
+  ];
 
   return (
     <section className="portfolio-section sec-padding">
@@ -31,45 +30,47 @@ const Portfolio = () => {
         </div>
 
         <div className="row">
+          <PortfolioItem data={data} />
           {/* portfolio item starts */}
-          <div className="portfolio-item">
+          {/* <div className="portfolio-item">
             <div className="portfolio-item-thumbnail">
               <img src={one} alt="portfolio item thumbnail" />
             </div>
-          </div>
-          <h3 className="portfolio-item-title">education course website</h3>
-          <button className="btn view-project-btn">view project</button>
-          <div className="description">
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque,
-              qui. Officia sint distinctio impedit veritatis aspernatur iusto
-              numquam quidem, maiores eum optio rem eos nobis nesciunt qui
-              dolorum maxime laboriosam.
-            </p>
-          </div>
-          <div className="general-info">
-            <ul>
-              <li>
-                Created - <span> 4 Dec 2020</span>
-              </li>
-              <li>
-                technologies used - <span>Html, Css</span>
-              </li>
-              <li>
-                Role - <span>Frontend</span>
-              </li>
-              <li>
-                View Online -{" "}
-                <span>
-                  {" "}
-                  <a href="#" target="_blank">
+
+            <h3 className="portfolio-item-title">education course website</h3>
+            <button className="btn view-project-btn">view project</button>
+            <div className="description">
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque,
+                qui. Officia sint distinctio impedit veritatis aspernatur iusto
+                numquam quidem, maiores eum optio rem eos nobis nesciunt qui
+                dolorum maxime laboriosam.
+              </p>
+            </div>
+            <div className="general-info">
+              <ul>
+                <li>
+                  Created - <span> 4 Dec 2020</span>
+                </li>
+                <li>
+                  technologies used - <span>Html, Css</span>
+                </li>
+                <li>
+                  Role - <span>Frontend</span>
+                </li>
+                <li>
+                  View Online -{" "}
+                  <span>
                     {" "}
-                    www.domain.com
-                  </a>{" "}
-                </span>
-              </li>
-            </ul>
-          </div>
+                    <a href="#" target="_blank">
+                      {" "}
+                      www.domain.com
+                    </a>{" "}
+                  </span>
+                </li>
+              </ul>
+            </div>
+          </div> */}
           {/* portfolio item ends */}
         </div>
       </div>
--- src/components/portfolio/PortfolioItem.js
+++ src/components/portfolio/PortfolioItem.js
@@ -1,49 +1,39 @@
 import React from "react";
 import one from "../../assets/portfolio/1.jpg";
 
-const PortfolioItem = ({}) => {
+const PortfolioItem = ({ data }) => {
+  console.log(data);
   return (
-    <div>
-      {" "}
-      <div className="portfolio-item">
-        <div className="portfolio-item-thumbnail">
-          <img src={one} alt="portfolio item thumbnail" />
-        </div>
-      </div>
-      <h3 className="portfolio-item-title">education course website</h3>
-      <button className="btn view-project-btn">view project</button>
-      <div className="description">
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque, qui.
-          Officia sint distinctio impedit veritatis aspernatur iusto numquam
-          quidem, maiores eum optio rem eos nobis nesciunt qui dolorum maxime
-          laboriosam.
-        </p>
-      </div>
-      <div className="general-info">
-        <ul>
-          <li>
-            Created - <span> 4 Dec 2020</span>
-          </li>
-          <li>
-            technologies used - <span>Html, Css</span>
-          </li>
-          <li>
-            Role - <span>Frontend</span>
-          </li>
-          <li>
-            View Online -{" "}
-            <span>
-              {" "}
-              <a href="#" target="_blank">
-                {" "}
-                www.domain.com
-              </a>{" "}
-            </span>
-          </li>
-        </ul>
-      </div>
-    </div>
+    <>
+      {data.map((item, index) => {
+        const { image, itemTitle, viewBtnPara, description, listItems } = item;
+        return (
+          <div key={index} className="portfolio-item">
+            <div className="portfolio-item-thumbnail">
+              <img src={one} alt="portfolio item thumbnail" />
+            </div>
+            <h3 className="portfolio-item-title"> {itemTitle} </h3>
+            <button className="btn view-project-btn"> {viewBtnPara} </button>
+            <div className="description">
+              <p>{description}</p>
+            </div>
+            <div className="general-info">
+              <ul>
+                {listItems.map((listItem, index) => {
+                  const { key, value } = listItem;
+                  return (
+                    <li key={index}>
+                      {key} <span>{value}</span>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>{" "}
+          </div>
+        );
+      })}
+      lol
+    </>
   );
 };
 
